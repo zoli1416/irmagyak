1a7f1cf2e9fe11d00c924631fa5b04ceb17d00e2
Green <Green@DESKTOP-OUHKF5I>
2020-11-30 20:31:22 +0100

    Solution created

---
+++ irmagyak/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace irmagyak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
