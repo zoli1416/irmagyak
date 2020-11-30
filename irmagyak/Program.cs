ec1b684d029bc902a83c5232926d5f61321c074c
Green <Green@DESKTOP-OUHKF5I>
2020-11-30 22:13:23 +0100

    basic function program

--- irmagyak/Program.cs
+++ irmagyak/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace irmagyak
 {
@@ -6,7 +8,61 @@ namespace irmagyak
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> inputs = new List<int>();
+            int checkNumber = -1;
+
+            Console.WriteLine("Enter 5 numbers to create a list of numbers:");
+
+            Console.WriteLine("Enter a number for the list:");
+            inputs.Add(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Enter a number for the list:");
+            inputs.Add(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Enter a number for the list:");
+            inputs.Add(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Enter a number for the list:");
+            inputs.Add(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Enter a number for the list:");
+            inputs.Add(Convert.ToInt32(Console.ReadLine()));
+
+
+            Console.WriteLine("Enter number to check things with it:");
+            checkNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (inputs.Contains(checkNumber))
+            {
+                Console.WriteLine("The list contains this number!");
+            }
+            else
+            {
+                Console.WriteLine("The list does not contains this number!");
+            }
+
+            if (inputs.Min(item => item) == checkNumber)
+            {
+                Console.WriteLine("The number is the smallest number in the list!");
+            }
+            else
+            {
+                Console.WriteLine("The number is not the smallest number in the list!");
+            }
+
+            if (inputs.Max(item => item) == checkNumber)
+            {
+                Console.WriteLine("The number is the biggest number in the list!");
+            }
+            else
+            {
+                Console.WriteLine("The number is not the biggest number in the list!");
+            }
+
+
+            Console.WriteLine("End of the program. Press enter to finish!");
+            Console.ReadLine();
+
         }
     }
 }
